e35d2df6fe8f311d7587f4169f6171983d79c552
Minh <minhta16@augustana.edu>
2019-04-07 13:41:09 -0500

    Basic drawer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import AppBar from "./layouts/AppBar.jsx";
+import SideBar from "./layouts/SideBar.jsx";
+import { List, ListItem, Switch, AppBar, CssBaseline } from "@material-ui/core";
+import "typeface-roboto";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: []
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SideBar />
       </div>
     );
   }
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import HomeIcon from "@material-ui/icons/Home";
+import PersonIcon from "@material-ui/icons/Person";
+
+const webRoutes = [
+  {
+    path: "/home",
+    name: "Home",
+    icon: HomeIcon
+    // component: HomePage
+  },
+  {
+    path: "/account-detail",
+    name: "Account Detail",
+    icon: PersonIcon
+    // component: HomePage
+  }
+];
+
+export default webRoutes;
